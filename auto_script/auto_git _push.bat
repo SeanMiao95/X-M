2d79c8811b965f8b4fd934e34fdaca95264a5a4f
Sean <1319587163@qq.com>
2018-12-10 21:49:30 +0800

    delete something

--- auto_script/auto_git _push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Users\HP\Desktop\XiaoaiM1
-git add .
-git commit -m "HuiHui add something new"
-git push origin master
--- auto_script/auto_git_pull.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\HP\Desktop\XiaoaiM1
-git pull origin master
